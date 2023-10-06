de978293c41079008e0c0576658ec1115b35658b
neostyle88 <makssimm1@gmail.com>
2023-10-06 12:04:49 +0200

    change names of variables

--- indicators.py
+++ indicators.py
@@ -7,14 +7,15 @@ from pandas import DataFrame
 class Indicator:
     def __init__(
         self,
-        obLevel: int,
-        osLevel: int,
+        ob_level: int,
+        os_level: int,
         length: int,
+        df: DataFrame
     ) -> None:
-        self.obLevel = obLevel
-        self.osLevel = osLevel
+        self.ob_level = ob_level
+        self.os_level = os_level
         self.length = length
-        self.df: DataFrame = pd.read_csv("binance_data.csv")
+        self.df = df
 
     def get_indicator(self) -> DataFrame:
         src = self.df["close"]
@@ -29,28 +30,28 @@ class Indicator:
         )
 
         self.df["x1"] = (self.length - 1) * (
-            adc * self.obLevel / (100 - self.obLevel) - auc
+            adc * self.ob_level / (100 - self.ob_level) - auc
         )
         self.df["x2"] = (self.length - 1) * (
-            adc * self.osLevel / (100 - self.osLevel) - auc
+            adc * self.os_level / (100 - self.os_level) - auc
         )
 
         self.df["ub"] = src + self.df["x1"].where(
             self.df["x1"] >= 0,
-            self.df["x1"] * (100 - self.obLevel) / self.obLevel,
+            self.df["x1"] * (100 - self.ob_level) / self.ob_level,
         )
         self.df["lb"] = src + self.df["x2"].where(
             self.df["x2"] >= 0,
-            self.df["x2"] * (100 - self.osLevel) / self.osLevel,
+            self.df["x2"] * (100 - self.os_level) / self.os_level,
         )
 
         return self.df
 
-    def build_plot(self) -> None:
-        plt.plot(self.df["ub"], label="Resistance", color="red", linewidth=2)
-        plt.plot(self.df["lb"], label="Support", color="green", linewidth=2)
+    def build_plot(self, indicators_df: DataFrame) -> None:
+        plt.plot(indicators_df["ub"], label="Resistance", color="red", linewidth=2)
+        plt.plot(indicators_df["lb"], label="Support", color="green", linewidth=2)
         plt.plot(
-            (self.df["ub"] + self.df["lb"]) / 2,
+            (indicators_df["ub"] + indicators_df["lb"]) / 2,
             label="RSI Midline",
             color="gray",
             linewidth=1,
@@ -61,10 +62,11 @@ class Indicator:
         plt.show()
 
 
-# if __name__ == "__main__":
-#     obLevel = 70
-#     osLevel = 30
-#     length = 14
-#     indicator = Indicator(obLevel, osLevel, length)
-#     indicator.get_indicator()
-#     indicator.build_plot()
+if __name__ == "__main__":
+    ob_level = 70
+    os_level = 30
+    length = 14
+    df = pd.read_csv("binance_data.csv")
+    indicator = Indicator(ob_level, os_level, length, df)
+    indicators_df = indicator.get_indicator()
+    indicator.build_plot(indicators_df)
