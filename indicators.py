56e17c89c3aacd4f4f9faee32815e2bc071ff73c
neostyle88 <makssimm1@gmail.com>
2023-10-06 08:41:28 +0200

    make indicators class based

--- indicators.py
+++ indicators.py
@@ -1,42 +1,66 @@
 import pandas as pd
 import talib
 import matplotlib.pyplot as plt
+from pandas import DataFrame
 
-df = pd.read_csv("binance_data.csv")
 
-obLevel = 70
-osLevel = 30
-length = 14
+class Indicator:
+    def __init__(
+        self,
+        obLevel: int,
+        osLevel: int,
+        length: int,
+    ) -> None:
+        self.obLevel = obLevel
+        self.osLevel = osLevel
+        self.length = length
+        self.df: DataFrame = pd.read_csv("binance_data.csv")
 
-src = df["close"]
+    def get_indicator(self) -> DataFrame:
+        src = self.df["close"]
 
-ep = 2 * length - 1
+        ep = 2 * self.length - 1
 
-auc = talib.EMA((src - src.shift(1)).clip(lower=0), timeperiod=ep)
-adc = talib.EMA((src.shift(1) - src).clip(lower=0), timeperiod=ep)
+        auc = talib.EMA((src - src.shift(1)).clip(lower=0), timeperiod=ep)
+        adc = talib.EMA((src.shift(1) - src).clip(lower=0), timeperiod=ep)
 
-df["x1"] = (length - 1) * (adc * obLevel / (100 - obLevel) - auc)
-df["x2"] = (length - 1) * (adc * osLevel / (100 - osLevel) - auc)
+        self.df["x1"] = (self.length - 1) * (
+            adc * self.obLevel / (100 - self.obLevel) - auc
+        )
+        self.df["x2"] = (self.length - 1) * (
+            adc * self.osLevel / (100 - self.osLevel) - auc
+        )
 
-df["ub"] = (
-        src + df["x1"].where(df["x1"] >= 0,
-                             df["x1"] * (100 - obLevel) / obLevel)
-)
-df["lb"] = (
-        src + df["x2"].where(df["x2"] >= 0,
-                             df["x2"] * (100 - osLevel) / osLevel)
-)
+        self.df["ub"] = src + self.df["x1"].where(
+            self.df["x1"] >= 0,
+            self.df["x1"] * (100 - self.obLevel) / self.obLevel,
+        )
+        self.df["lb"] = src + self.df["x2"].where(
+            self.df["x2"] >= 0,
+            self.df["x2"] * (100 - self.osLevel) / self.osLevel,
+        )
 
-if __name__ == '__main__':
-    plt.plot(df['ub'], label="Resistance", color='red', linewidth=2)
-    plt.plot(df['lb'], label="Support", color='green', linewidth=2)
-    plt.plot(
-        (df['ub'] + df['lb']) / 2,
-        label="RSI Midline",
-        color='gray',
-        linewidth=1
-    )
-    plt.title("RSI Bands")
-    plt.legend()
+        return self.df
 
-    plt.show()
+    def build_plot(self) -> None:
+        plt.plot(self.df["ub"], label="Resistance", color="red", linewidth=2)
+        plt.plot(self.df["lb"], label="Support", color="green", linewidth=2)
+        plt.plot(
+            (self.df["ub"] + self.df["lb"]) / 2,
+            label="RSI Midline",
+            color="gray",
+            linewidth=1,
+        )
+        plt.title("RSI Bands")
+        plt.legend()
+
+        plt.show()
+
+
+# if __name__ == "__main__":
+#     obLevel = 70
+#     osLevel = 30
+#     length = 14
+#     indicator = Indicator(obLevel, osLevel, length)
+#     indicator.get_indicator()
+#     indicator.build_plot()
